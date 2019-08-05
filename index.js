bce0d7a006708282fd9ed6ce871e64c746cdfbe1
Edinho Rodrigues <edinho_rodrigues@hotmail.com>
2019-08-05 10:13:26 -0300

    teste

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ setInterval(function() {
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/sw.js')
+           .register('add-home-screen/sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
