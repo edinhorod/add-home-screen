c4834a05e96adac3c44f02481aeb1ed3e52e4b74
Edinho Rodrigues <edinho_rodrigues@hotmail.com>
2019-08-05 11:44:10 -0300

    teste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/src/registerServiceWorker.js')
+        .register('/service-worker.js')
         .then(function () { console.log('Service Worker Registered'); });
 }
 
