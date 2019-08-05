7afba9393840f2f6513f3e83792a241e1fb344be
Edinho Rodrigues <edinho_rodrigues@hotmail.com>
2019-08-05 10:23:51 -0300

    teste

--- sw.js
+++ sw.js
@@ -2,14 +2,14 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       'add-home-screen/',
-       'add-home-screen/index.html',
-       'add-home-screen/index.js',
-       'add-home-screen/style.css',
-       'add-home-screen/images/fox1.jpg',
-       'add-home-screen/images/fox2.jpg',
-       'add-home-screen/images/fox3.jpg',
-       'add-home-screen/images/fox4.jpg'
+       '/add-home-screen/',
+       '/add-home-screen/index.html',
+       '/add-home-screen/index.js',
+       '/add-home-screen/style.css',
+       '/add-home-screen/images/fox1.jpg',
+       '/add-home-screen/images/fox2.jpg',
+       '/add-home-screen/images/fox3.jpg',
+       '/add-home-screen/images/fox4.jpg'
      ]);
    })
  );
